02de058d277a254e700a2a96a1684d6f126ea8d4
Squarewave34 <132561675+Squarewave34@users.noreply.github.com>
2024-12-18 16:30:18 +0300

    routes work

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import NavBar from "./components/NavBar";
+import { Route, Routes } from 'react-router-dom';
+import MailboxDetails from "./components/MailboxDetails"
+import MailboxForm from './components/MailboxForm'
+import MailboxList from './components/MailboxList'
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <h1>The root component that orchestrates the entire application</h1>
+      <Routes>
+        <Route path="/" element={
+          <h1>home</h1>
+        }/>
+        <Route path="/mailboxes" element={
+          <h1>mailboxes</h1>
+        }/>
+        <Route path="/mailboxForm" element={
+          <h1>mailboxForm</h1>
+        }/>
+        <Route path="*" element={
+          <h1>Mailbox Not Found!</h1>
+        }/>
+      </Routes>
     </>
   )
 };
