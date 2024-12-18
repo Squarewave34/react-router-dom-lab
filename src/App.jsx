07042fdac8e3f2f44c61e8f3ea3540f001844f3b
Squarewave34 <132561675+Squarewave34@users.noreply.github.com>
2024-12-18 16:37:17 +0300

    finished following the assignment instructions, the rest i will figure out on my own

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import NavBar from "./components/NavBar";
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import MailboxDetails from "./components/MailboxDetails"
 import MailboxForm from './components/MailboxForm'
 import MailboxList from './components/MailboxList'
 
 const App = () => {
+  const mailboxExample = {
+    _id: 1,
+    boxSize: 'Small',
+    boxHolder: 'Alex',
+  };
+  
+
+  const [mailbox, setMailbox]=useState([mailboxExample])
+
+  const addMailbox= (newMailbox)=>{
+    newMailbox._id=mailbox.length+1
+    setMailbox([...mailbox, newMailbox])
+  }
+  
   return (
     <>
       <NavBar />
@@ -13,10 +28,10 @@ const App = () => {
           <h1>home</h1>
         }/>
         <Route path="/mailboxes" element={
-          <h1>mailboxes</h1>
+          <MailboxList />
         }/>
         <Route path="/mailboxForm" element={
-          <h1>mailboxForm</h1>
+          <MailboxForm />
         }/>
         <Route path="*" element={
           <h1>Mailbox Not Found!</h1>
--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -1,4 +1,10 @@
 const MailboxDetails = () =>{
+
+  const { mailboxId } = useParams();
+  const selectedBox = props.mailboxes.find(
+    (mailbox) => mailbox._id === Number(mailboxId)
+  );
+
   return(
     <h1>Shows details about a specific mailbox</h1>
   )
