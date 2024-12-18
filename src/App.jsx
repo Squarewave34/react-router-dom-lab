ea7caf8cc37a1bc662a0cbf70385d773d7a5ed3e
Squarewave34 <132561675+Squarewave34@users.noreply.github.com>
2024-12-18 16:23:47 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import NavBar from "./components/NavBar";
+
 const App = () => {
   return (
-    <h1>The root component that orchestrates the entire application</h1>
+    <>
+      <NavBar />
+      <h1>The root component that orchestrates the entire application</h1>
+    </>
   )
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = ()=>{
   return (
-    <h1>Provides navigation links</h1>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/mailboxes">MailboxList</Link></li>
+        <li><Link to="/mailboxForm">MailboxForm</Link></li>
+      </ul>
+    </nav>
   )
 }
 
